565925bc34850ac25db50a9e9979a732f54b6602
chetna <cheta.gup@gmail.com>
2013-04-01 05:40:14 +0530

    changed integrate_primitive_polynomial from recursive to iterative

--- sympy/integrals/risch.py
+++ sympy/integrals/risch.py
@@ -943,42 +943,49 @@ def canonical_representation(a, d, DE):
 
 def hermite_reduce(a, d, DE):
     """
-    Hermite Reduction - Mack's Linear Version.
+Hermite Reduction - Quadratic version.
 
-    Given a derivation D on k(t) and f = a/d in k(t), returns g, h, r in
-    k(t) such that f = Dg + h + r, h is simple, and r is reduced.
-
-    """
+Given a derivation D on k(t) and f = a/d in k(t), returns g, h, r in
+k(t) such that f = Dg + h + r, h is simple, and r is reduced.
+"""
+    # TODO: Rewrite this using Mack's linear version
+    # Make d monic
     l = Poly(1/d.LC(), DE.t)
     a, d = a.mul(l), d.mul(l)
 
     fp, fs, fn = canonical_representation(a, d, DE)
+
     a, d = fn
     l = Poly(1/d.LC(), DE.t)
     a, d = a.mul(l), d.mul(l)
+
+    d_sqf = d.sqf_list_include()
     ga = Poly(0, DE.t)
     gd = Poly(1, DE.t)
-    dd=derivation(d,DE)
-    dm=gcd(d,dd).as_poly(DE.t)
-    ds,r =d.div(dm)
-    while dm.degree(DE.t)>0:
-        ddm=derivation(dm,DE)
-        dm2=gcd(dm,ddm)
-        dms, r = dm.div(dm2)
-        ds_ddm=ds.mul(ddm)
-        ds_ddm_dm,r = ds_ddm.div(dm)
-        b,c=gcdex_diophantine(-ds_ddm_dm.as_poly(DE.t),dms.as_poly(DE.t),a.as_poly(DE.t))
-        b, c = b.as_poly(DE.t), c.as_poly(DE.t)
-        db=derivation(b,DE).as_poly(DE.t)
-        ds_dms,r=ds.div(dms)
-        a=c.as_poly(DE.t)-db.mul(ds_dms).as_poly(DE.t)
-        ga=ga*dm+b*gd
-        gd=gd*dm
-        dm=dm2
-    d=ds
+
+    for v, i in d_sqf:
+        if i < 2:
+            continue
+
+        u = d.exquo(v**i)
+        for j in range(i - 1, 0, -1):
+            udv = u*derivation(v, DE)
+            b, c = gcdex_diophantine(udv.as_poly(DE.t), v.as_poly(DE.t),
+                a.mul(Poly(-S(1)/j, DE.t)).as_poly(DE.t))
+            b, c = b.as_poly(DE.t), c.as_poly(DE.t)
+
+            vj = v**j
+            ga = ga*vj + b*gd
+            gd = gd*vj
+            a = c.mul(Poly(-j, DE.t)) - u*derivation(b, DE)
+
+        d = u*v
+
     q, r = a.div(d)
+
     ga, gd = ga.cancel(gd, include=True)
     r, d = r.cancel(d, include=True)
+
     rra = q*fs[1] + fp*fs[1] + fs[0]
     rrd = fs[1]
     rra, rrd = rra.cancel(rrd, include=True)
@@ -1126,31 +1133,34 @@ def integrate_primitive_polynomial(p, DE):
     from sympy.integrals.prde import limited_integrate
 
     Zero = Poly(0, DE.t)
-
+    q = Zero
     if not p.has(DE.t):
         return (Zero, p, True)
 
-    Dta, Dtb = frac_in(DE.d, DE.T[DE.level - 1])
+    while True:
+        if not p.has(DE.t):
+            return (q, p, True)
 
-    with DecrementLevel(DE):  # We had better be integrating the lowest extension (x)
+        Dta, Dtb = frac_in(DE.d, DE.T[DE.level - 1])
+
+        with DecrementLevel(DE):  # We had better be integrating the lowest extension (x)
                               # with ratint().
-        a = p.LC()
-        aa, ad = frac_in(a, DE.t)
+            a = p.LC()
+            aa, ad = frac_in(a, DE.t)
 
-        try:
-            (ba, bd), c = limited_integrate(aa, ad, [(Dta, Dtb)], DE)
-            assert len(c) == 1
-        except NonElementaryIntegralException:
-            return (Zero, p, False)
+            try:
+                (ba, bd), c = limited_integrate(aa, ad, [(Dta, Dtb)], DE)
+                assert len(c) == 1
+            except NonElementaryIntegralException:
+                return (q, p, False)
 
-    m = p.degree(DE.t)
-    q0 = c[0].as_poly(DE.t)*Poly(DE.t**(m + 1)/(m + 1), DE.t) + \
-        (ba.as_expr()/bd.as_expr()).as_poly(DE.t)*Poly(DE.t**m, DE.t)
+        m = p.degree(DE.t)
+        q0 = c[0].as_poly(DE.t)*Poly(DE.t**(m + 1)/(m + 1), DE.t) + \
+            (ba.as_expr()/bd.as_expr()).as_poly(DE.t)*Poly(DE.t**m, DE.t)
 
-    # TODO: Rewrite this non-recursively
-    # c.f. risch_integrate(log(x)**1001, x)
-    q, r, b = integrate_primitive_polynomial(p - derivation(q0, DE), DE)
-    return (q + q0, r, b)
+        p = p - derivation(q0, DE)
+        q = q + q0
+    return (q, r, b)
 
 
 def integrate_primitive(a, d, DE, z=None):
